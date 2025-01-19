57c8eb06de74d17dff4cb84e5c665eedc9e6a7cd
Eunna <160303427+Eunnaeooi@users.noreply.github.com>
2025-01-19 16:01:27 +0900

    Update data_loader.py
    aws db 연결

--- HybridRecommender/app/data_loader.py
+++ HybridRecommender/app/data_loader.py
@@ -1,23 +1,22 @@
 import pandas as pd
 from sqlalchemy import create_engine
-from app.config import host, port, user, password , database, charset
+import cryptography
+from app.config import host, port, user, password, database, charset
 
 # SQLAlchemy 엔진 생성
 engine = create_engine(
     f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}?charset={charset}"
 )
 
-# 데이터 로드 함수 (MySQL에서 불러오기)
 def load_data():
     
-    Recipe = pd.read_sql("SELECT * FROM Recipe", engine)
-    User_Cuisine = pd.read_sql("SELECT * FROM User_Cuisine", engine)
-    User_FoodType = pd.read_sql("SELECT * FROM User_FoodType", engine)
-    User_CookingStyle = pd.read_sql("SELECT * FROM User_CookingStyle", engine)
-    User_Scrap = pd.read_sql("SELECT * FROM User_Scrap", engine)
+    Recipe = pd.read_sql("SELECT * FROM recipe", engine)
+    User_Cuisine = pd.read_sql_query("SELECT * FROM user_cuisine", engine)
+    User_FoodType = pd.read_sql_query("SELECT * FROM user_foodtype", engine)
+    User_CookingStyle = pd.read_sql_query("SELECT * FROM user_cookingstyle", engine)
+    User_Scrap = pd.read_sql_query("SELECT * FROM user_scrap", engine)
     
     return Recipe, User_Cuisine, User_FoodType, User_CookingStyle, User_Scrap
-
 
 
 '''
