5efbf7623c8fbf2b02da68505d8e53cb37238888
Eunna <160303427+Eunnaeooi@users.noreply.github.com>
2025-01-19 16:00:28 +0900

    Update config.py
    aws db

--- HybridRecommender/app/config.py
+++ HybridRecommender/app/config.py
@@ -1,3 +1,12 @@
+# aws MySQL 연결 정보
+host = "reciguard-db.czouas0eqnqo.ap-northeast-2.rds.amazonaws.com"
+port = 3306
+user = 'reciguard'
+password = 'tavereciguard'
+database = 'reciguard'
+charset = 'utf8mb4'
+
+'''
 # MySQL 연결 정보
 host = 'localhost'
 port = 3306
@@ -5,6 +14,7 @@ user = 'root'
 password = 'danchoo12'
 database = 'RECIGUARD'
 charset = 'utf8mb4'
+'''
 
 # 하이퍼파라미터
 WEIGHT_CUISINE = 0.5
